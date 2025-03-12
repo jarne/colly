e133bef32f5b08dc3777b22aa97fa1ac41a9e2ad
Jarne <dev@jarne.rocks>
2025-03-12 18:48:09 +0100

    Change env variables for Vite

--- client/src/util/InternalAPI.js
+++ client/src/util/InternalAPI.js
@@ -3,7 +3,7 @@
  */
 
 class InternalAPI {
-    static API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT || "/api"
+    static API_ENDPOINT = import.meta.env.REACT_APP_API_ENDPOINT || "/api"
 }
 
 export default InternalAPI
