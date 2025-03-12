d107d453e045bc975b9dc6b8636c6c7d5327f97c
Jarne <dev@jarne.rocks>
2025-03-12 19:18:01 +0100

    Don't rebuild client on every Docker container start

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -2,13 +2,8 @@
 
 # Commands to execute when starting Colly Docker container
 
-# Build client to have up-to-date customization with env variables
-cd client
-yarn build
-cd ..
-
 # Run initial user creation check script
-node userInit.js
+yarn node userInit.js
 
 # Run Node.js application
 yarn start
