e8609e98305ffa906cd628835c82b515678af8fa
Jarne <dev@jarne.rocks>
2025-08-08 15:36:58 +0200

    Add tests for password change and log-out routes

--- test/route/authTest.js
+++ test/route/authTest.js
@@ -17,13 +17,16 @@ const CORRECT_USER = `${TEST_PREFIX}the_rockstar22`
 const CORRECT_PASSWORD = "C0mpl3xP@55"
 
 describe("auth router", () => {
+    let uid
+
     before(function (done) {
         const prepare = async () => {
-            await user.create({
+            const createdUser = await user.create({
                 username: CORRECT_USER,
                 password: CORRECT_PASSWORD,
                 isAdmin: false,
             })
+            uid = createdUser.id
 
             done()
         }
@@ -81,6 +84,28 @@ describe("auth router", () => {
         })
     })
 
+    describe("post /api/auth/logout", () => {
+        it("should log out successfully", async () => {
+            const authRes = await request(app)
+                .post("/api/auth/login")
+                .set("Content-Type", "application/json")
+                .send({
+                    username: CORRECT_USER,
+                    password: CORRECT_PASSWORD,
+                })
+
+            const token = authRes.body.token
+
+            const res = await request(app)
+                .post("/api/auth/logout")
+                .set("Content-Type", "application/json")
+                .set("Authorization", `Bearer ${token}`)
+                .send()
+
+            expect(res.status).to.eq(204)
+        })
+    })
+
     describe("get /api/auth/me", () => {
         it("should return user object", async () => {
             const authRes = await request(app)
@@ -127,9 +152,90 @@ describe("auth router", () => {
         })
     })
 
-    after(async () => {
-        await User.findOneAndDelete({
-            username: /^test-route-auth-.*/,
+    describe("patch /api/me/passwordChange", () => {
+        it("should change password successfully", async () => {
+            const CHANGE_USERNAME = `${TEST_PREFIX}tmp-music_lover123`
+            const CHANGE_OLD_PASSWORD = "C0mpl3xP@55"
+            const CHANGE_NEW_PASSWORD = "P@ssw0rd1234"
+
+            await user.create({
+                username: CHANGE_USERNAME,
+                password: CHANGE_OLD_PASSWORD,
+                isAdmin: false,
+            })
+
+            const authRes = await request(app)
+                .post("/api/auth/login")
+                .set("Content-Type", "application/json")
+                .send({
+                    username: CHANGE_USERNAME,
+                    password: CHANGE_OLD_PASSWORD,
+                })
+            const token = authRes.body.token
+
+            const res = await request(app)
+                .patch("/api/auth/me/passwordChange")
+                .set("Content-Type", "application/json")
+                .set("Authorization", `Bearer ${token}`)
+                .send({
+                    existingPassword: CHANGE_OLD_PASSWORD,
+                    newPassword: CHANGE_NEW_PASSWORD,
+                })
+
+            expect(res.status).to.eq(204)
+
+            const newPwRes = await request(app)
+                .post("/api/auth/login")
+                .set("Content-Type", "application/json")
+                .send({
+                    username: CHANGE_USERNAME,
+                    password: CHANGE_NEW_PASSWORD,
+                })
+
+            expect(newPwRes.status).to.eq(200)
         })
+
+        it("should fail with wrong existing password", async () => {
+            const CHANGE_USERNAME = `${TEST_PREFIX}tmp-tech_guru88`
+            const CHANGE_OLD_PASSWORD = "StrongP@ssw0rd!"
+            const CHANGE_NEW_PASSWORD = "P@ssw0rd1234"
+
+            await user.create({
+                username: CHANGE_USERNAME,
+                password: CHANGE_OLD_PASSWORD,
+                isAdmin: false,
+            })
+
+            const authRes = await request(app)
+                .post("/api/auth/login")
+                .set("Content-Type", "application/json")
+                .send({
+                    username: CHANGE_USERNAME,
+                    password: CHANGE_OLD_PASSWORD,
+                })
+            const token = authRes.body.token
+
+            const res = await request(app)
+                .patch("/api/auth/me/passwordChange")
+                .set("Content-Type", "application/json")
+                .set("Authorization", `Bearer ${token}`)
+                .send({
+                    existingPassword: "Wrong@ssw0rd!",
+                    newPassword: CHANGE_NEW_PASSWORD,
+                })
+
+            expect(res.status).to.eq(400)
+            expect(res.body.error.code).to.eq("existing_password_incorrect")
+        })
+    })
+
+    afterEach(async () => {
+        await User.findOneAndDelete({
+            username: /^test-route-auth-tmp-.*/,
+        })
+    })
+
+    after(async () => {
+        await User.findByIdAndDelete(uid)
     })
 })
